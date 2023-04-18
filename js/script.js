77125a67ec8ec864f137d6bc340100d545cd4c89
danicaclarize <100770381+danicaclarize@users.noreply.github.com>
2023-04-18 17:27:11 +0800

    java

--- js/script.js
+++ js/script.js
@@ -1,7 +1,23 @@
-// Change cursor to a pointer
-document.body.style.cursor = "pointer";
+const cursor = document.createElement('div'); //creating a new div element to be used as custom curson
+cursor.className = 'cursor';
+document.body.appendChild(cursor); // appends the element to CSS cursor element to the DOM tree
 
-// Change name to typewriter mode
-setInterval(function() {
-  document.write("<pre>Danica Carlos</pre>");
-}, 100);
+
+
+const typewriter = document.getElementById('typewriter');
+const text = "Danica Carlos";
+var index = 0;
+
+
+// Write the function
+function typeWriter() {
+  if (index < text.length) {
+    typewriter.textContent += text.charAt(index);
+    index++;
+    setTimeout(typeWriter, 150);
+  }
+}
+
+typeWriter();
+
+cursor.style.backgroundImage = 'url("https://custom-cursor.com/thumbnail/cursor/thumbnail20333.png")';
