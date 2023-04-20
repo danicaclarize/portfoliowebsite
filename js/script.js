68813102801e081c439b56265836ed3b2b10a632
danicaclarize <100770381+danicaclarize@users.noreply.github.com>
2023-04-20 20:13:48 +0800

    added JS

--- js/script.js
+++ js/script.js
@@ -6,3 +6,30 @@ menuIcon.onclick = () =>{
 menuIcon.classList.toggle('bx-x');
 navbar.classList.toggle('active');
 };
+
+
+
+// Scroll Section Active Link
+var sections = document.querySelectorAll('section');
+var navLinks = document.querySelectorAll('header nav a');
+
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        var top = window.scrollY;
+        var offset = sec.offsetTop -150;
+        var height = sec.offsetHeight;
+        var id = sec.getAttribute('id');
+
+
+        if(top >= offset && top < offset + height) {
+
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+
+        };
+
+    });
+};
