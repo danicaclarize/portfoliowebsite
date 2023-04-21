6b202e51a03dc6b3e492bc5855c3ff60b90e0914
danicaclarize <100770381+danicaclarize@users.noreply.github.com>
2023-04-22 00:02:51 +0800

    edited syntax in JS

--- js/script.js
+++ js/script.js
@@ -45,4 +45,4 @@ ScrollReveal({
 
 
 ScrollReveal().reveal('.home-content, .heading', {origin:'top }');
-ScrollReveal().reveal('.home-img, .portfolio-box, .contact form', {origin:'bottom'}');
+ScrollReveal().reveal('.home-img, .portfolio-box, .contact form', {origin:'bottom'});
